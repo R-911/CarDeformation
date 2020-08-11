3dd4bf54fc682eefd4244147697fe266d70dca80
R-911 <68863802+R-911@users.noreply.github.com>
2020-08-11 11:51:36 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var car, wall;
+var speed, weight;
+
+function setup() {
+  createCanvas(1200,400);
+ // createSprite(400, 200, 50, 50);
+
+  wall = createSprite(1100, 200, 60, height/2);
+  car = createSprite(50, 200, 50, 50);
+  speed = random(55, 90);
+  weight = random(400, 1500);
+  car.velocityX = speed;
+}
+
+function draw() {
+  background("blue");  
+
+   //wall.shapeColour(80, 80, 80);
+
+  if(car.isTouching(wall)){
+    car.velocityX = 0;
+    var deformation = 0.5*weight*speed*speed/22500;
+    
+    if(deformation<= 100){
+      car.shapeColor = "green";
+    }
+    if(deformation> 100 && deformation<=180){
+      car.shapeColor = "yellow";
+    }
+    if(deformation> 180){
+      car.shapeColor = "red";
+    }
+  }
+  drawSprites();
+}
